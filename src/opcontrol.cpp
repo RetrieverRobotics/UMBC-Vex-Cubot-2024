b21c2f9dd9c5667b462c0e0362e1c3c78662cf7b
jkieltyka15 <jmkielty@mtu.edu>
2024-02-13 19:36:15 -0500

    CUBOT #1 - Implemented drive controls to be arcade style.

--- src/opcontrol.cpp
+++ src/opcontrol.cpp
@@ -15,19 +15,63 @@ using namespace pros;
 using namespace umbc;
 using namespace std;
 
+#define MOTOR_RED_GEAR_MULTIPLIER    100
+#define MOTOR_GREEN_GEAR_MULTIPLIER  200
+#define MOTOR_BLUE_GEAR_MULTIPLIER   600
+#define MOTOR_REVERSE                true
+
+// ports for left drive
+#define LEFT_FRONT_MOTOR_PORT 17
+#define LEFT_MIDDLE_MOTOR_PORT 18
+#define LEFT_BACK_MOTOR_PORT  19
+
+// ports for right drive
+#define RIGHT_FRONT_MOTOR_PORT 8
+#define RIGHT_MIDDLE_MOTOR_PORT 9
+#define RIGHT_BACK_MOTOR_PORT  10
+
+// ports for lift
+#define LIFT_MOTOR_PORT 5
+
 void umbc::Robot::opcontrol() {
 
     // nice names for controllers (do not edit)
     umbc::Controller* controller_master = this->controller_master;
     umbc::Controller* controller_partner = this->controller_partner;
 
-    // initialize motors and sensors
+    // initialize left drive
+    pros::Motor drive_left_front_motor = pros::Motor(LEFT_FRONT_MOTOR_PORT, MOTOR_REVERSE);
+    pros::Motor drive_left_middle_motor = pros::Motor(LEFT_MIDDLE_MOTOR_PORT);
+	pros::Motor drive_left_back_motor = pros::Motor(LEFT_BACK_MOTOR_PORT, MOTOR_REVERSE);
+    pros::MotorGroup drive_left = 
+        pros::MotorGroup(vector<pros::Motor>{drive_left_front_motor, drive_left_middle_motor, drive_left_back_motor});
+    drive_left.set_brake_modes(E_MOTOR_BRAKE_COAST);
+    drive_left.set_gearing(E_MOTOR_GEAR_GREEN);
+	
+    // initialize right drive
+    pros::Motor drive_right_front_motor = pros::Motor(RIGHT_FRONT_MOTOR_PORT, MOTOR_REVERSE);
+    pros::Motor drive_right_middle_motor = pros::Motor(RIGHT_MIDDLE_MOTOR_PORT);
+	pros::Motor drive_right_back_motor = pros::Motor(RIGHT_BACK_MOTOR_PORT, MOTOR_REVERSE);
+    pros::MotorGroup drive_right =
+        pros::MotorGroup(vector<pros::Motor>{drive_right_front_motor, drive_right_middle_motor, drive_right_back_motor});
+    drive_right.set_brake_modes(E_MOTOR_BRAKE_COAST);
+    drive_right.set_gearing(E_MOTOR_GEAR_GREEN);
 
 
     while(1) {
 
-        // implement opcontrols
+        // set velocity for drive (arcade controls)
+        int32_t arcade_y = controller_master->get_analog(E_CONTROLLER_ANALOG_LEFT_Y);
+        int32_t arcade_x = controller_master->get_analog(E_CONTROLLER_ANALOG_LEFT_X);
 
+        int32_t drive_left_velocity = -(int32_t)(((double)(arcade_x + arcade_y) / (double)E_CONTROLLER_ANALOG_MAX)
+                                        * MOTOR_GREEN_GEAR_MULTIPLIER);
+
+        int32_t drive_right_velocity = -(int32_t)(((double)(arcade_x - arcade_y) / (double)E_CONTROLLER_ANALOG_MAX)
+                                        * MOTOR_GREEN_GEAR_MULTIPLIER);                                
+
+        drive_left.move_velocity(drive_left_velocity);
+        drive_right.move_velocity(drive_right_velocity);
 
         // required loop delay (do not edit)
         pros::Task::delay(this->opcontrol_delay_ms);
